6b6379013144b71d0ca7759bf28ea411f420e6b2
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2024-05-15 11:50:06 -0300

    Ajustes na estilização da tela de cadastro efetuado com sucesso

--- cadastro.php
+++ cadastro.php
@@ -1,27 +1,48 @@
 <?php
-
 include('conexao.php');
 
-// Atribuindo os valores corretos às variáveis
-$nome = $_POST['nome'];
-$telefone = $_POST['telefone'];
-$cpf = $_POST['cpf'];
-$rg = $_POST['rg'];
-$data_nascimento = $_POST['data_nascimento'];
-$username = $_POST['username'];
-$email = $_POST['email'];
-$endereco = $_POST['endereco'];
-$senha = $_POST['senha'];
-$confirmar_senha = $_POST['confirmar_senha'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Coletar os dados do formulário
+    $nome = $_POST["nome"];
+    $cpf = $_POST["cpf"];
+    $rg = $_POST["rg"];
+    $data_nascimento = $_POST["data_nascimento"];
+    $genero = $_POST["genero"];
+    $username = $_POST["usuario"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    $endereco = $_POST["endereco"];
+    $senha = $_POST["senha"];
+    
+    // Inserir dados no banco de dados
+    $sql = "INSERT INTO clientes(nome, telefone, cpf, rg, data_nascimento, genero, username, email, endereco, senha) 
+            VALUES ('$nome', '$telefone', '$cpf', '$rg', '$data_nascimento', '$genero', '$username', '$email', '$endereco', '$senha')";
 
-// Inserir dados no banco de dados
-$sql = "INSERT INTO clientes(nome, telefone, cpf, rg, data_nascimento, username, email, endereco, senha) 
-        VALUES ('$nome', '$telefone', '$cpf', '$rg', '$data_nascimento', '$username', '$email', '$endereco', '$senha')";
+    if (mysqli_query($conexao, $sql)) {
+        echo "<div class='container-logotipo'>";
+            echo "<img src='./assets/logotipo_digitalhurus.png' alt='Logotipo Digital Hurus'>";
+        echo "</div>";
 
-if (mysqli_query($conexao, $sql)) {
-    echo "Cliente cadastrado com sucesso!!!";
-} else {
-    echo "Erro ao cadastrar o cliente: " . mysqli_error($conexao);
+            echo "<h2 class='txt-cadastro-efetuado'>Cadastro foi realizado com sucesso! ✅</h2>";
+        echo "<p class='txt-aviso'>Para voltar a tela de cadastro, <a class='link-redirecionamento' href='cadastrar_cliente.php'>clique aqui</a></p>";
+    } else {
+        echo "Erro ao cadastrar o cliente: " . mysqli_error($conexao);
+    }
+
+    mysqli_close($conexao); // Fechar a conexão apenas após o processamento dos dados
 }
+?>
 
-mysqli_close($conexao);
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digital Hurus | Cadastro</title>
+    <link rel="shortcut icon" href="/assets/logotipo_digitalhurus.png" type="image/x-icon">
+    <link rel="stylesheet" href="cadastro.css">
+</head>
+<body>
+    
+</body>
+</html>
